14f5b5d57d0438247b1a2c6c613495ad3726dcf0
aleido <ridho1030@gmail.com>
2019-10-08 14:23:49 +0700

    awalan

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pendaftaran Pegawai Baru | Sorabel CAbang Pingit</title>
+</head>
+
+<body>
+	<header>
+		<h3>Pendaftaran Pegawai Baru</h3>
+		<h1>Sorabel Pingit</h1>
+	</header>
+	
+	<h4>Menu</h4>
+	<nav>
+		<ul>
+			<li><a href="form-daftar.php">Daftar Baru</a></li>
+			<li><a href="list-siswa.php">Pendaftar</a></li>
+		</ul>
+	</nav>
+	
+	
+	<?php if(isset($_GET['status'])): ?>
+	<p>
+		<?php
+			if($_GET['status'] == 'sukses'){
+				echo "Pendaftaran pegawai baru berhasil!";
+			} else {
+				echo "Pendaftaran gagal!";
+			}
+		?>
+	</p>
+	<?php endif; ?>
+	
+	</body>
+</html>
